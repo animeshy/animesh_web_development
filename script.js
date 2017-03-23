74098abe19daaf4af019cdc3e0aa77dfcdc1fe3c
animeshy <animeshy@gmail.com>
2017-03-24 03:22:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded",
         
         // Call server to get the name
         $ajaxUtils
-          .sendGetRequest("C:name.json", 
+          .sendGetRequest("name.json", 
             function (res) {
               var message = 
                 res.firstName + " " + res.lastName
