3b8ae2e2d49ea895c12f318dfc2349744a14ecf7
animeshy <animeshy@gmail.com>
2017-03-31 10:18:36 +0530

    Update assignment1.php

--- assignment1.php
+++ assignment1.php
@@ -1,32 +1,11 @@
- <?php
- //read data from form
- $lName = filter_input(INPUT_POST, "lName");
- $fName = filter_input(INPUT_POST, "fName");
- $email = filter_input(INPUT_POST, "email");
- $phone = filter_input(INPUT_POST, "phone");
- //print form results to user
- print <<< HERE
- <h1>Thanks!</h1>
- <p>
-  Your spam will be arriving shortly.
- </p>
- <p>
- first name: $fName <br />
- last name: $lName <br />
- email: $email <br/>
- phone: $phone
- </p>
-HERE;
- //generate output for text file
- $output = <<< HERE
-first: $fName
-last: $lName
-email: $email
-phone: $phone
-HERE;
- //open file for output
- $fp = fopen("contacts.txt", "a");
- //write to the file
- fwrite($fp, $output);
- fclose($fp);
- ?>
+<?php
+$name=$_POST['name'];
+$email=$_POST['email'];
+echo '<h1>Your File has been added to contacts.txt</h1>';
+$myfile = fopen("contacts.txt", "w") or die("Unable to open file!");
+$txt = $name . "\n";
+fwrite($myfile, $txt);
+$txt = $email;
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
